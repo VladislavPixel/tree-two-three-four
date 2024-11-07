412f8727e6b9c26fbd91a456b69a7fc4aa6ddd6b
VladislavPixel <pixel.world7@mail.ru>
2024-11-07 17:40:56 +0300

    create methods for node-tree and tree

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+// Класс узла для многопутевых деревьев
 class NodeMultipathTree {
-	constructor() {
-		this.arrData = new Array(3);
-		this.arrChildren = new Array(4);
+	constructor(maxAmountDataForNode) {
+		this.arrData = new Array(maxAmountDataForNode);
+		this.arrChildren = new Array(maxAmountDataForNode + 1);
 		this.parent = null;
+		this.length = 0;
+	};
+
+	isEmpty() {
+		return this.length === 0;
+	};
+
+	isFull() {
+		return this.length === this.arrData.length;
 	};
 
 	display() {
@@ -16,10 +26,85 @@ class NodeMultipathTree {
 
 		console.log(value);
 	};
+
+	addData(newValue) {
+		let m = this.length;
+
+		for (; m > 0; m--) {
+			const value = this.arrData[m - 1];
+
+			if (newValue >= value) {
+				break;
+			}
+
+			this.addData[m] = this.arrData[m - 1];
+		}
+
+		this.addData[m] = newValue;
+
+		this.length = this.length + 1;
+
+		return m;
+	};
+
+	isComplete() {
+		return this.isFull();
+	};
+
+	isLeaf() {
+		return (this.arrChildren[0] === undefined) || (this.arrChildren[0] === null);
+	};
+
+	split() {
+		const rightBrother = new NodeMultipathTree(this.arrData.length);
+		const saveMidValue = this.arrData[1];
+		const saveRightValue = this.arrData[2];
+
+		let parent = this.parent;
+
+		// Если ссылки на родительский узел нет, то мы пытаемся разбить корень дерева - это частный случай
+		if (parent === null) {
+			const newParent = new NodeMultipathTree(this.arrData.length);
+
+			parent = newParent;
+		}
+
+		
+	};
 };
 
 class TreeTwoThreeFour {
 	constructor() {
-		
+		this.root = null;
+	};
+
+	isEmpty() {
+		return this.root === null;
+	};
+
+	insert(newValue) {
+		// Если дерево пустое, то мы добавляем в узел элемент и делаем этот узел корнем
+		if (this.isEmpty()) {
+			const newNode = new NodeMultipathTree(3);
+
+			newNode.addData(newValue);
+
+			this.root = newNode;
+
+			return true;
+		}
+
+		// Подготовили ссылку для передвижения по дереву
+		let current = this.root;
+
+		while(true) {
+			// Если узел полон, то его требуется разбить перед тем, как что-то делать дальше
+			if (current.isComplete()) {
+				current = current.split();
+
+			} else {
+
+			}
+		}
 	};
 };
